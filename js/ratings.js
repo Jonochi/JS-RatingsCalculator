e4158eb9ce549c62f760f05fda67d6347c932407
Tana Larrabee <tana.jayne@gmail.com>
2022-06-14 14:00:04 -0600

    step 9

--- js/ratings.js
+++ js/ratings.js
@@ -1,6 +1,11 @@
 function collect_ratings(){
-    const ratings = { 'count' : 0, 'sum' : 0, 'average' : 0 }
-    let rating = 0
+    const ratings = { 'count' : 0, 'sum' : 0, 'average' : 0 };
+    let rating = 0;
 }
 
 const elements = document.querySelectorAll('.rating');
+elements.forEach(element => {
+    rating = parseInt(element.id.replace( 'star', ''));
+    ratings.count += parseInt(element.value);
+    ratings.sum += parseInt(element.value) * rating;
+});
