51318a409f18b9044183aa1259a5b07e74678c8c
Tana Larrabee <tana.jayne@gmail.com>
2022-06-14 14:08:24 -0600

    step 10

--- js/ratings.js
+++ js/ratings.js
@@ -10,6 +10,8 @@ elements.forEach(element => {
     ratings.sum += parseInt(element.value) * rating;
 });
 
-if (ratings.count !== 0){
+if (ratings.count !== 0) {
+    ratings.average = ratings.sum / ratings.count;
+} 
 
-}
+return ratings;
